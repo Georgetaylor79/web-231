8b6345c9e845e9ee35b3f408cac32c0f8afa722d
George <george_rtaylor@yahoo.com>
2023-08-22 02:48:11 -0500

    commit

---
+++ week-2/hello-world.js
@@ -0,0 +1,10 @@
+/*
+============================================
+; Title:  hello-world.js
+; Author: George Taylor
+; Date:   8.20.2023
+; Description: Displays message to the console window
+;===========================================
+*/
+
+console.log("Hello World!");
